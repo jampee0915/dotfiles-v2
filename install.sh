600d75092270280bbd37a85a1ba61ee14dbd8e5c
jampee <jumpei.toyoda@flyle.io>
2024-03-19 23:14:05 +0900

    fix

--- install.sh
+++ install.sh
@@ -4,4 +4,6 @@ DOT_DIR="$HOME/Downloads"
 
 curl -L -o ${DOT_DIR}/dotfiles-script https://github.com/jampee0915/dotfiles-v2/releases/download/v0.0.0/main
 chmod +x ${DOT_DIR}/dotfiles-script
-${DOT_DIR}/dotfiles-script
+
+cd ${DOT_DIR}
+./dotfiles-script
