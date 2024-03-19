585401ae4b335df133dddc350767bee6dca447d5
jampee <jumpei.toyoda@flyle.io>
2024-03-19 23:18:25 +0900

    fix

--- install.sh
+++ install.sh
@@ -2,9 +2,11 @@
 
 DOT_DIR="$HOME/Downloads"
 
+# バイナリファイルをダウンロード
 curl -L -o ${DOT_DIR}/dotfiles-script https://github.com/jampee0915/dotfiles-v2/releases/download/v0.0.0/main
+
+# 実行権限を付与
 chmod +x ${DOT_DIR}/dotfiles-script
 
-cd ${DOT_DIR}
-./dotfiles-script
-touch sample.txt
+# 実行
+${DOT_DIR}/dotfiles-script
