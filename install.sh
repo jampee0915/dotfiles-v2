312c89e661e9235d151a9a9d7a0c315b8bcba830
jampee <jumpei.toyoda@flyle.io>
2024-03-19 23:11:37 +0900

    add install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+DOT_DIR="$HOME/Downloads"
+
+curl -L -o ${DOT_DIR}/dotfiles-script https://github.com/jampee0915/dotfiles-v2/releases/download/v0.0.0/main
+chmod +x ${DOT_DIR}/dotfiles-script
