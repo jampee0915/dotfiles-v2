7093f619b7ac0ccff2a47a8525cbe17ccba904ab
jampee <jumpei.toyoda@flyle.io>
2024-03-20 22:14:36 +0900

    fix

--- tools/brew.go
+++ tools/brew.go
@@ -21,6 +21,42 @@ func installHomebrew() error {
 	return nil
 }
 
+func addTap() error {
+	taps := []string{
+		"homebrew/bundle",
+		"homebrew/cask",
+		"homebrew/cask-versions",
+		"homebrew/core",
+	}
+
+	for _, tap := range taps {
+		if err := exec.Command("brew", "tap", tap).Run(); err != nil {
+			return fmt.Errorf("failed to add tap %s: %w", tap, err)	
+		}
+	}
+	return nil
+}
+
+func installCaskApp() error {
+	caskApps := []string{
+		"iterm2",
+		"docker",
+		// "corretto",
+		"visual-studio-code",
+		"karabiner-elements",
+		"hyperswitch",
+	}
+
+	for _, app := range caskApps {
+		fmt.Println("Installing:", app)
+		if err := exec.Command("brew", "install", "--cask", app).Run(); err != nil {
+			return fmt.Errorf("failed to cask app %s: %w", app, err)		
+		}
+	}
+
+	return nil
+}
+
 func installBrewPackages() error {
 	brewPackages := []string{
 		"ghq", "jq", "peco", "tmux", "zsh", "zplug",
@@ -50,8 +86,16 @@ func InstallBrew() error {
 		return fmt.Errorf("failed to verify Homebrew installation")
 	}
 
+	if err := addTap(); err != nil {
+		return fmt.Errorf("failed to add brew tap")
+	}
+
 	if err := installBrewPackages(); err != nil {
-		return err
+		return fmt.Errorf("failed to brew package installation")
+	}
+
+	if err := installCaskApp(); err != nil {
+		return fmt.Errorf("failed to cask app installation")
 	}
 
 	return nil
